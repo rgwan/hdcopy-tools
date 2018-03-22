01afbb54a8a9e08563dbb9b1861b386621f0c4e2
Zhiyuan Wan <h@iloli.bid>
2018-03-21 21:58:48 -0400

    Fix missing track and accept manually adjust output file size

--- decompress.c
+++ decompress.c
@@ -9,16 +9,21 @@ int main(int argc, char *argv[])
 	uint8_t *hdcopy;
 	uint8_t *plain;
 	uint32_t length;
+	uint32_t fit_size = 0;
 
 	int i, j, k, r;
 
-	if(argc != 3)
+	if(argc != 3 && argc != 4)
 	{
-		fprintf(stderr, "Usage: %s <HD-COPY Image> <IMA plain floppy image>\n\n", argv[0]);
+		fprintf(stderr, "Usage: %s <HD-COPY Image> <IMA plain floppy image> [size]\n\n", argv[0]);
 		fprintf(stderr, "This small tool can decompress HD-COPY image to plain floppy image.\n");
 		fprintf(stderr, "It can help you to use old HD-COPY image on modern PC,\nespecially on Virtual Machine to test out old-school softwares.\n\n");
 		fprintf(stderr, "Zhiyuan Wan <h@iloli.bid> 2018, License WTFPL.\nAlgorithm analyze from <https://github.com/ciel-yu/devnotes>. Thanks him!\n");
 		exit(-1);
+	}
+	if(argc == 4)
+	{
+		fit_size = atoi(argv[3]);
 	}
 	fp = fopen(argv[1], "rb");
 	if(!fp)
@@ -36,7 +41,7 @@ int main(int argc, char *argv[])
 
 	fread(hdcopy, length, 1, fp);
 
-	printf("Decoding HD-COPY Image\nInput size = %d\n", length);
+	printf("Decompressing HD-COPY Image\nInput size = %d\n", length);
 	fclose(fp);
 
 	uint8_t *actualimage;
@@ -59,48 +64,43 @@ int main(int argc, char *argv[])
 	int maxTrackCount = actualimage[0];
 	int secPerTrack = actualimage[1];
 
-	printf("maxTrackCount = %d, secPerTrack = %d\n", maxTrackCount, secPerTrack);
+	printf("maxTrackCount = %d, secPerTrack = %d\n", maxTrackCount + 1, secPerTrack);
 	uint8_t *dataTrack = actualimage + 2; /* 有数据的磁道表 */
 
-	uint8_t *decode_p = plain;
+	uint8_t *decomp_p = plain;
 
 	memset(plain, 0x00, 0x168000);
 
-	for(i = 0; i < maxTrackCount; i++)
+	for(i = 0; i <= maxTrackCount; i++)
 	{
 		for(j = 0; j < 2; j++)
 		{
 			if(dataTrack[(i * 2) + j] != 0x01)
 			{
-				decode_p += 512 * secPerTrack;
+				decomp_p += 512 * secPerTrack;
 				continue;
 			}
 			int dataLen = payload[0] + (payload[1] << 8);
 			payload += 2;
 			uint8_t escByte; /* RLE 压缩 */
-			for(k = 0; k < dataLen; k++)
+
+			escByte = payload[0];
+			for(k = 1; k < dataLen; k++)
 			{
-				if(k == 0)
+				if(payload[k] == escByte)
 				{
-					escByte = payload[0];
+					k++;
+					uint8_t repeatByte = payload[k++];
+					int repeat = payload[k];
+
+					for(r = 0; r < repeat; r++)
+					{
+							*(decomp_p++) = repeatByte;
+					}
 				}
 				else
 				{
-					if(payload[k] == escByte)
-					{
-						k++;
-						uint8_t repeatByte = payload[k++];
-						int repeat = payload[k];
-
-						for(r = 0; r < repeat; r++)
-						{
-							*(decode_p++) = repeatByte;
-						}
-					}
-					else
-					{
-						*(decode_p++) = payload[k];
-					}
+					*(decomp_p++) = payload[k];
 				}
 			}
 			payload += dataLen;
@@ -108,8 +108,8 @@ int main(int argc, char *argv[])
 	}
 	uint16_t secCount = plain[0x13] + (plain[0x14] << 8);
 	printf("Floppy sector count = %d, fitting to %d bytes\n",
-		   secCount, secCount * 512);
-	printf("Decode successfully, write it to file\n");
+		   secCount, fit_size > 0 ? fit_size : secCount * 512);
+	printf("Decompress operation completed, write it to file\n");
 	fp = fopen(argv[2], "wb+");
 	if(!fp)
 	{
@@ -117,7 +117,7 @@ int main(int argc, char *argv[])
 		goto deal;
 	}
 	fseek(fp, 0, SEEK_SET);
-	fwrite(plain, secCount * 512 > 0 ? secCount * 512 : 0x168000, 1, fp);
+	fwrite(plain, fit_size > 0 ? fit_size : (secCount * 512 > 0 ? secCount * 512 : 0x168000), 1, fp);
 	fclose(fp);
 
 deal:
